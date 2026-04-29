fa2db49f9c6538c9405c0712af8f5f9824318de8
Bong Bungalan Jr <bbungalan@gmail.com>
2026-04-29 22:06:52 +0800

    implement: PWA config and manifests

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your existing nextConfig here
+  reactStrictMode: true,
+  turbopack: {},
+};
+
+export default withPWA(nextConfig);
